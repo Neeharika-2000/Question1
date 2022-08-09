1d61a88b5af1161805d97a004f4f808985262311
Dasari Neeharika <Neeharika.Dasari@ivycomptech.com>
2022-08-09 12:34:43 +0530

    changes done

---
+++ len_String.java
@@ -0,0 +1,10 @@
+ import java.util.Scanner;
+    public class len_String{
+        public static void main(String args[]){
+            String s1="Neeharika";
+            String s2="jyothi";
+            System.out.println("string length is: "+s1.length()+ " ");
+            System.out.println("string length is: "+s2.length());
+        }
+    }
+
